541729fea7506339bed82668d41aeb730f41bb66
Theekshana1999 <theekshana.amamadu@gmail.com>
2025-10-29 22:05:01 +0530

    setup server.js file

--- backend/server.js
+++ backend/server.js
@@ -26,6 +26,9 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json());
 
 const studentRoutes = require("./routes/studentRoutes.js");
+const adminRoutes = require("./routes/adminRoutes.js");
+
+app.use("/api/admins", adminRoutes);
 
 app.use("/api/students", studentRoutes);
 
