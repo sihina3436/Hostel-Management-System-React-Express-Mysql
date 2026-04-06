3eb1784132d0dd383a35a2b46ff2421f80fb9725
sihina3436 <nimnadasihina@gmail.com>
2026-04-07 02:39:52 +0530

    feat: implement student authentication, routes, and update controller/model
    - Added studentAuth middleware
    - Updated student controller logic
    - Modified student model
    - Added student routes

--- backend/models/student.model.js
+++ backend/models/student.model.js
@@ -1,6 +1,6 @@
 const { DataTypes } = require("sequelize");
 const sequelize = require("../configurations/dbconnection.js");
-const Rooms = require( "./room.model.js");
+const Rooms = require("./room.model.js");
 
 const Student = sequelize.define("Student", {
   stud_id: { type: DataTypes.INTEGER, autoIncrement: true, primaryKey: true },
@@ -8,9 +8,15 @@ const Student = sequelize.define("Student", {
   phone_no: { type: DataTypes.STRING },
   address: { type: DataTypes.STRING },
   email: { type: DataTypes.STRING, unique: true },
+  role: { type: DataTypes.STRING, allowNull: false ,defaultValue:'student'},
+  password: {
+  type: DataTypes.STRING,
+  allowNull: false
+},
 });
 
-Rooms.hasOne(Student, { foreignKey: "room_id" });
+
+Rooms.hasMany(Student, { foreignKey: "room_id" });
 Student.belongsTo(Rooms, { foreignKey: "room_id" });
 
 module.exports = Student;
