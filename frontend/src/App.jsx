a1b67eeefe2e9cca962a70ed883865d27a26bfa4
Theekshana1999 <theekshana.amamadu@gmail.com>
2026-04-08 01:31:17 +0530

    feat: update App and main entry components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,18 @@
-import React from 'react'
-import Home from './Home'
-import NavBar from './Components/NavBar'
-import Footer from './Components/Footer'
-import { Outlet } from 'react-router-dom'
+import React from "react";
+import { Outlet } from "react-router-dom";
+import Navbar from "./Components/NavBar";
+import Footer from "./Components/Footer";
 
 const App = () => {
   return (
-    <div className="flex flex-col min-h-screen bg-white">
-      <NavBar />
+    <div className="min-h-screen flex flex-col bg-slate-950">
+      <Navbar />
       <main className="flex-1">
         <Outlet />
       </main>
       <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
